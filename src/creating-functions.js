706953dd7c6ef5587934570816b9f21cae997a12
Max V Olofsson <max.olofsson@se.experis.com>
2024-10-24 10:47:12 +0200

    Done with core

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function incrementByOne(num) {
+  return num + 1;
+}
 
 // 2. Define a function that capitalises any string
 
@@ -16,6 +19,9 @@
 // hello | Hello
 // world | World
 // Hello | Hello
+function capitalizeString(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 // TODO: write code below
 
@@ -29,6 +35,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function greetPerson(name) {
+  return `Hi, ${capitalizeString(name)} :)`;
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +50,9 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function countStrings(arr) {
+  return arr.filter(item => typeof item === 'string').length;
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +64,18 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addEdwardProperty(obj) {
+  if (!obj.edward) {
+    obj.edward = 'amazing';
+  }
+  return obj;
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalizeString, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: greetPerson, // etc
+  d: countStrings,
+  e: addEdwardProperty
 }
